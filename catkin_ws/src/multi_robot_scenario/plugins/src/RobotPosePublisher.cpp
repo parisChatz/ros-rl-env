c06f49ba7c5b708e58b7ecb7d3946346a6422935
Mariusz-ubuntu <m.wisniewski@cranfield.ac.uk>
2025-03-17 08:32:10 +0000

    updated to publish velocity

--- catkin_ws/src/multi_robot_scenario/plugins/src/RobotPosePublisher.cpp
+++ catkin_ws/src/multi_robot_scenario/plugins/src/RobotPosePublisher.cpp
@@ -2,6 +2,8 @@
 #include <gazebo/common/Plugin.hh>
 #include <gazebo/physics/physics.hh>
 #include <geometry_msgs/Pose.h>
+#include <gazebo_msgs/ModelState.h>
+
 
 namespace gazebo
 {
@@ -12,6 +14,7 @@ namespace gazebo
     event::ConnectionPtr updateConnection;
     ros::NodeHandle* nh;
     ros::Publisher pose_pub;
+    ros::Publisher twist_pub;
 
   public:
     void Load(physics::ModelPtr _parent, sdf::ElementPtr /*_sdf*/)
@@ -24,6 +27,7 @@ namespace gazebo
     //         double update_rate = _sdf->Get<double>("update_rate");
     //     }
       pose_pub = nh->advertise<geometry_msgs::Pose>("/robot_pose", 100);
+      twist_pub = nh->advertise<geometry_msgs::Twist>("/robot_twist", 100);
 
       updateConnection = event::Events::ConnectWorldUpdateBegin(
           std::bind(&RobotPosePublisher::OnUpdate, this));
@@ -31,7 +35,10 @@ namespace gazebo
 
     void OnUpdate()
     {
+      gazebo_msgs::ModelState state_msg;
       geometry_msgs::Pose pose_msg;
+      geometry_msgs::Twist twist_msg;
+
       pose_msg.position.x = model->WorldPose().Pos().X();
       pose_msg.position.y = model->WorldPose().Pos().Y();
       pose_msg.position.z = model->WorldPose().Pos().Z();
@@ -40,7 +47,25 @@ namespace gazebo
       pose_msg.orientation.z = model->WorldPose().Rot().Z();
       pose_msg.orientation.w = model->WorldPose().Rot().W();
 
+      twist_msg.linear.x = model->WorldLinearVel()[0];
+      twist_msg.linear.y = model->WorldLinearVel()[1];
+      twist_msg.linear.z = model->WorldLinearVel()[2];
+      twist_msg.angular.x = model->WorldAngularVel()[0];
+      twist_msg.angular.y = model->WorldAngularVel()[1];
+      twist_msg.angular.z = model->WorldAngularVel()[2];
+
+
+
+      // twist_mgs.linear.x = model->WorldPose().Twist().linear().X()
+      // twist_mgs.linear.x = model->WorldPose().Twist().linear().Y()
+      // twist_mgs.linear.x = model->WorldPose().Twist().linear().Z()
+      // twist_mgs.linear.x = model->WorldPose().Twist().linear().X()
+      // twist_mgs.linear.x = model->WorldPose().Twist().linear().X()
+      // twist_mgs.linear.x = model->WorldPose().Twist().linear().X()
+
+
       pose_pub.publish(pose_msg);
+      twist_pub.publish(twist_msg);
     }
   };
 
